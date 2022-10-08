630f45b05a6d36b4903d8ea6bf118641fbfa93b2
Hexade <85897199+superboom12s@users.noreply.github.com>
2022-10-09 01:29:36 +0200

    Add files via upload

---
+++ spambotWhatsap.py
@@ -0,0 +1,12 @@
+import pyautogui, webbrowser
+
+                                                    # |
+                                                    # V phone number (entire number without spaces)
+webbrowser.open('https://web.whatsapp.com/send?phone=+00000000000')
+
+
+
+while True:
+    pyautogui.typewrite('spam ;)')
+    pyautogui.press('enter')
+
